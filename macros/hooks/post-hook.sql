14f0dde140ff265f5f47fa2df2b72bad487de153
Rodrigo Carneiro <teoria@gmail.com>
2026-02-02 13:18:58 -0300

    Enhance logging for table metrics SQL macro

--- macros/hooks/post-hook.sql
+++ macros/hooks/post-hook.sql
@@ -2,18 +2,27 @@
 
     {% if table_name is string %}
         {% if table_name.split(".")|length == 3 %}
+             {{ log ( "_______STRING_3___________", false) }}
             {% set   catalog=table_name.split(".")[0] %} 
             {% set   schema=table_name.split(".")[1] %} 
             {% set   table_name=table_name.split(".")[2]   %}
-        {% else %}
 
+        {% elif table_name.split(".")|length == 2 %}
+            {{ log ( "_______STRING_2___________", false) }}
+            {% set   catalog= database %} 
+            {% set   schema=table_name.split(".")[0] %} 
+            {% set   table_name=table_name.split(".")[1]   %}
+        
+        {% elif table_name.split(".")|length == 2 %}
             {% set rel = ref(table_name).render().replace('"','').split(".") %}
             {{ log ( ref(table_name).render().schema), false }}
             {{ log ( ref(table_name).render()), false }}
-            {{ log ( "_______STRING____________"), false }}
+            {{ log ( "_______STRING_1___________", false) }}
             {% set catalog = rel[0] %}
             {% set schema = rel[1] %}
             {% set table_name = rel[2] %}
+         
+             
         {% endif %}
         
         {{ return(adapter.dispatch('get_table_metrics_sql', 'iceberg_utils')(catalog,schema,table_name)) }}
@@ -21,7 +30,7 @@
         {% set catalog = table_name.database %}
         {% set schema = table_name.schema %}
         {% set table_name = table_name.name %}
-        {{ log ( "_______MODEL____________"), false }}
+        {{ log ( "_______MODEL____________", false) }}
         {% if model.config.table_type == 'iceberg' %}  
             {{ return(adapter.dispatch('get_table_metrics_sql', 'iceberg_utils')(catalog,schema,table_name)) }}
         {% endif %}
